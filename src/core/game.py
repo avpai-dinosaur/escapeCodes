839ffee960810bd101b834754d7617b6ab48423b
Dakshesh Daruri <dvdaruri@umich.edu>
2025-08-31 18:29:54 -0400

    walking sounds done

--- src/core/game.py
+++ src/core/game.py
@@ -1,4 +1,5 @@
 from src.core.camera import Camera
+from src.core.soundManager import SoundManager
 from src.core.ecodeEvents import EventManager, EcodeEvent
 from src.core.uiManager import UiManager
 from src.core.level import LevelFactory, Level 
@@ -12,6 +13,7 @@ class Game():
         self.manager = manager
         self.camera = Camera()
         self.uiManager = UiManager()
+        self.soundManager = SoundManager()
         self.isPaused = False
         self.levelName = levelName
         self.currentLevel: Level = LevelFactory.create(levelName)
--- src/core/soundManager.py
+++ src/core/soundManager.py
@@ -1,4 +1,5 @@
 import pygame
+from src.core.ecodeEvents import EventManager, EcodeEvent
 
 class SoundManager:
     def __init__(self):
@@ -6,35 +7,67 @@ class SoundManager:
 
         # Preload sound effects
         self.sounds = {
-            "run": pygame.mixer.Sound("assets/sounds/Footstep_Left_Stone.ogg"),
-            "walk": pygame.mixer.Sound("assets/sounds/Footstep_Right_Stone.ogg"),
+            "walk": pygame.mixer.Sound("assets/sounds/Footstep_Left_Stone.ogg")
         }
+        self.sounds["walk"].set_volume(0.5)
+        self.walk_channel = pygame.mixer.Channel(1)
 
         # Background music files
         self.music_tracks = {
             "menu": "assets/music/MenuMusic.mp3",
             "level1": "assets/music/level1_theme.mp3",
         }
+        
 
-        # Set volume defaults
-        for sound in self.sounds.values():
-            sound.set_volume(0.5)  # 50% volume
+        '''
+         #music changes
+        EventManager.subscribe(EcodeEvent.PLAYER_DIED, self.on_death)
+        EventManager.subscribe(EcodeEvent.LEVEL_ENDED, self.next_level)
+        EventManager.subscribe(EcodeEvent.PAUSE_MENU, self.pause_menu)
+        EventManager.subscribe(EcodeEvent.ENTERED_DANCE_FLOOR, self.dance(True))
+        EventManager.subscribe(EcodeEvent.LEFT_DANCE_FLOOR, self.dance(False))
+        '''
+       
+
+        #sound effects
+        EventManager.subscribe(EcodeEvent.PLAYER_MOVED, self.moved)
+        self.last_step_time = 0
+        self.step_delay = 0.1  # seconds between steps
+        '''
+        EventManager.subscribe(EcodeEvent.CAMERA_ALARM, self.alarm)
+        EventManager.subscribe(EcodeEvent.CAMERA_SHAKE, self.takeoff)
+        EventManager.subscribe(EcodeEvent.OPEN_DOOR, self.door_opening)
+
+        #boss stuff
+        EventManager.subscribe(EcodeEvent.START_BOSS_FIGHT, self.boss_fight)
+        EventManager.subscribe(EcodeEvent.START_BOSS_FIGHT, self.boss_charge)
+        EventManager.subscribe(EcodeEvent.START_BOSS_FIGHT, self.boss_attack)
+        EventManager.subscribe(EcodeEvent.START_BOSS_FIGHT, self.boss_death)
+        '''
+
 
     def play_sound(self, name: str):
         if name in self.sounds:
             self.sounds[name].play()
 
-    def play_music(self, name: str, loops=-1):
+    def play_music(self, name: str, loop=True):
         if name in self.music_tracks:
-            pygame.mixer.music.load(self.music_tracks[name])
-            pygame.mixer.music.play(loops)
+            if self.current_music != name:  # avoid reloading same track
+                pygame.mixer.music.stop()
+                pygame.mixer.music.load(self.music_tracks[name])
+                loops = -1 if loop else 0
+                pygame.mixer.music.play(loops=loops)
+                self.current_music = name
+    
+    def moved(self, target: pygame.Rect):
+        now = pygame.time.get_ticks()
+        if now - self.last_step_time >= self.step_delay:
+            if not self.walk_channel.get_busy():
+                self.walk_channel.play(self.sounds["walk"])
+            self.last_step_time = now
+
 
     def stop_music(self):
         pygame.mixer.music.stop()
 
-    def set_music_volume(self, volume: float):
-        pygame.mixer.music.set_volume(volume)
-
-    def set_sound_volume(self, name: str, volume: float):
-        if name in self.sounds:
-            self.sounds[name].set_volume(volume)
+    
